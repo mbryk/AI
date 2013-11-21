9ef181aef7ed0b6a287b32a80a0a8c655dd3d309
Mark Bryk <bryk@cooper.edu>
2013-11-21 00:56:47 -0500

    Revert "new project"
    This reverts commit 3c8d763c0bbca7d90e7625dac8ea1df2d88c1535.

--- main.cpp
+++ main.cpp
@@ -1,57 +1,68 @@
-int main(int argc, char **argv){
-	int nodeAmts[3] = {inputs,hiddens,outputs};
-	int L1[inputs][hiddens]; // Weights
-	int L2[hiddens][outputs]; //Weights
+/**************
+Mark Bryk
+ECE 469 - Project 1
+Intelligent Checkers
+November 3rd, 2013
+**************/
 
-	int i,j;
-	for(j=0;j<hiddens;j++){
-		for(i=0;i<inputs;i++){
-			L1[i][j] = rand();	
-		} 
-		for(k=0;k<outputs;k++){
-			L2[j][k] = rand();
+#include "game.h"
+#include "unistd.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+const char *parseFile(char *fileName)
+{
+	ifstream in(fileName);
+	if(!in.good()){
+		cerr<<"Bad Board File"<<endl; exit(-1);
+	}
+	string contents((std::istreambuf_iterator<char>(in)), 
+    istreambuf_iterator<char>());
+	return contents.c_str();
+}
+
+int main(int argc, char *argv[]){
+	const char *state = (char*) "1111.1111.1111.----.----.2222.2222.2222";
+	int players[2] = {1,0};
+	double t_limit = 5;
+	int hnum[2] = {2,2};
+	int c = 0;
+	while ((c = getopt (argc, argv, "b:p:h:")) != -1){
+		switch (c){
+			case 'b':
+				state = parseFile(optarg);
+				break;
+			case 'p':
+				players[0] = optarg[0] - '0';
+				players[1] = optarg[1] - '0';
+				break;
+			case 'h':
+				hnum[0] = optarg[0] - '0';
+				hnum[1] = optarg[1] - '0';
+				break;
 		}
 	}
-	for(e=0;e<length(examples);e++){
-		for(i=0;i<length(x);i++){
-			node[0][i]->activation = x[i];
-		}
-		for(l=1;l<3;l++){
-			for(j=0;j<nodeAmts[l];j++){
-				node[l][j]->in = 0;
-				for(i=0;i<nodeAmts[l-1];i++){
-					node[l][j]->in += L[i][j]*node[l-1][i]->activation;
-				}
-				node[l][j]->activation = g(in);
-			}
-		}
-		for(j=0;j<nodeAmts[2];j++){
-			node[2][j]->delta = gp(node[2][j]->in)*y[j]-node[2][j]->activation;
-		}
-		for(l=1;l>0;l--){
-			for(i=0;i<nodeAmts[l];i++){
-				delta = 0;
-				for(j=0;j<nodeAmts[l+1];j++){
-					delta += L[i][j] * node[l+1][j]->delta;
-				}
-				node[l][i]->delta = gp(node[2][j]->in)*delta;
-			}
-		}
-		for(j=0;j<hiddens;j++){
-			for(i=0;i<inputs;i++){
-				L1[i][j] += alpha * node[0][i]->activation * node[1][i]->delta;
-			} 
-			for(k=0;k<outputs;k++){
-				L2[j][k] += alpha * node[1][j]->activation * node[2][k]->delta;
-			}
-		}
-	}
-}
-int g(int in){
+	
+	char turnc; string t_limit_string;
+	int turn;
+	int t_limit_int;
+	cout<<"Whose turn is it?"<<endl<<"1:Red, 2:Black"<<endl;
+	do{
+		cin>>turnc;
+		turn = turnc - '0' - 1;
+	} while (turn!=0 && turn!=1);
 
-	return 0;
-}
-int gprime(int in){
+	cout<<"What is the program's time limit? (in seconds)"<<endl;
+	do{
+		cin>>t_limit_string;
+		t_limit_int = atoi(t_limit_string.c_str());
+	} while(t_limit_int<=0);
+	t_limit = (double) t_limit_int;
+
+	Game *game = new Game(state, players, hnum, t_limit);
+	game->play(turn);	
 
 	return 0;
 }
